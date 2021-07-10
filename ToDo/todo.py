984cab032dbdab6a80cbd2cfb1cf93c38eac6abf
Ji Myung Hwa <battlesun99@gmai.com>
2021-07-11 01:19:20 +0900

    todo list with swagger

--- ToDo/todo.py
+++ ToDo/todo.py
@@ -4,11 +4,15 @@ from flask_restx import Resource, Api, Namespace
 todos = {}
 count = 1
 
-todo_ns = Namespace('Todo')
+todo_ns = Namespace(
+    name="MyungHwa's TODO API",
+    description="나만의 TODO 리스트를 위한 API"
+)
 
 @todo_ns.route('')
 class TodoPost(Resource):
     def post(self):
+        """ TODO 리스트 등록 """
         global count
         global todos
 
@@ -21,12 +25,15 @@ class TodoPost(Resource):
 @todo_ns.route('/<int:todo_id>')
 class TodoSimple(Resource):
     def get(self, todo_id):
+        """ TODO 리스트 조회 """
         return {'todo_id': todo_id, 'data': todos[todo_id]}
     
     def put(self, todo_id):
+        """ TODO 리스트 수정 """
         todos[todo_id] = request.json.get('data')
         return {'todo_id': todo_id, 'data': todos[todo_id]}
         
     def delete(self, todo_id):
+        """ TODO 리스트 삭제 """
         del todos[todo_id]
         return {'delete': "success"}
--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_restx import Api, Resource
-from ToDo.todo import Todo
+from jinja2.utils import contextfunction
+from ToDo.todo import todo_ns
 
 app = Flask(__name__)
-api = Api(app)
+api = Api(
+    app=app,
+    verision='0.1',
+    title="API Server",
+    description="TODO API Server",
+    terms_url='/',
+    contact="battlesun99@gmail.com",
+    license="MIT"
+)
 
-api.add_namespace(Todo, '/todos')
+api.add_namespace(todo_ns, '/todos')
 
 if __name__ == "__main__":
     app.run(debug=True, host='localhost', port=5000)
