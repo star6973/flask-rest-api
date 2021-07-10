01899032672cd6737aa4eec82b802a4df5fb0498
Ji Myung Hwa <battlesun99@gmai.com>
2021-07-11 00:57:22 +0900

    rest api + swagger

--- database/db_access.py
+++
@@ -1,3 +0,0 @@
-def access_data():
-    result = {}
-    
--- rest_client/example.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, request
-
-exam_bp = Blueprint("example", __name__)
-
-@exam_bp.route("/hello")
-def print_hello():
-    return "Hello"
