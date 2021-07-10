18f3ce9ea5f126c5b6cdcb95d32988dd36b8b007
Ji Myung Hwa <battlesun99@gmai.com>
2021-07-11 01:12:03 +0900

    make simple todolist

---
+++ ToDo/todo.py
@@ -0,0 +1,32 @@
+from flask import request
+from flask_restx import Resource, Api, Namespace
+
+todos = {}
+count = 1
+
+todo_ns = Namespace('Todo')
+
+@todo_ns.route('')
+class TodoPost(Resource):
+    def post(self):
+        global count
+        global todos
+
+        idx = count
+        count += 1
+        todos[idx] = request.json.get('data')
+
+        return {'todo_id': idx, 'data': todos[idx]}
+
+@todo_ns.route('/<int:todo_id>')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {'todo_id': todo_id, 'data': todos[todo_id]}
+    
+    def put(self, todo_id):
+        todos[todo_id] = request.json.get('data')
+        return {'todo_id': todo_id, 'data': todos[todo_id]}
+        
+    def delete(self, todo_id):
+        del todos[todo_id]
+        return {'delete': "success"}
--- app.py
+++ app.py
@@ -1,78 +1,11 @@
-from flask import Flask, request
-from flask_restplus import Api, Resource, fields
+from flask import Flask
+from flask_restx import Api, Resource
+from ToDo.todo import Todo
 
 app = Flask(__name__)
-api = Api(app, version="1.0", title="상품 관리 API", description="상품 등록, 수정, 삭제, 조회 API")
-ns = api.namespace("goods", description="상품 등록, 수정, 삭제, 조회")
+api = Api(app)
 
-# refine data model
-model_goods = api.model('row_goods', {
-    'id': fields.Integer(readOnly=True, required=True, description="상품번호"),
-    'goods_name': fields.String(required=True, description="상품명")
-})
-
-class GoodsDAO(object):
-    ''' 상품 정보 데이터 접근 객체 '''
-    def __init__(self):
-        self.counter = 0
-        self.rows = []
-
-    ''' 상품 정보 조회 '''
-    def get(self, id):
-        for row in self.rows:
-            if row['id'] == id:
-                return row
-        api.abort(404, "{} doesn't exist".format(id))
-
-    ''' 상품 정보 등록 '''
-    def create(self, data):
-        row = data
-        row['id'] = self.counter = self.counter + 1
-        self.rows.append(row)
-        return row
-
-    ''' 상품 정보 수정 '''
-    def update(self, id, data):
-        row = self.get(id)
-        row.update(data)
-        return row
-
-    ''' 상품 정보 삭제 '''
-    def delete(self, id):
-        row = self.get(id)
-        self.rows.remove(row)
-
-DAO = GoodsDAO()
-DAO.create({'goods_name': '삼성 갤럭시북 Pro'})
-DAO.create({'goods_name': 'LG Gram'})
-
-@ns.route('/')
-class GoodsListManager(Resource):
-    @ns.marshal_list_with(model_goods)
-    def get(self):
-        return DAO.rows
-
-    @ns.expect(model_goods)
-    @ns.marshal_with(model_goods, code=201)
-    def post(self):
-        return DAO.create(api.payload), 201 
-
-@ns.route('/<int:id>')
-@ns.response(404, 'id를 찾을 수가 없습니다')
-@ns.param('id', '상품 번호를 입력하세요')
-class GoodsRunManager(Resource):
-    @ns.marshal_with(model_goods)
-    def get(self, id):
-        return DAO.get(id)
-
-    def delete(self, id):
-        DAO.delete(id)
-        return '', 200
-
-    @ns.expect(model_goods)
-    @ns.marshal_with(model_goods)
-    def put(self, id):
-        return DAO.update(id, api.payload)
+api.add_namespace(Todo, '/todos')
 
 if __name__ == "__main__":
     app.run(debug=True, host='localhost', port=5000)
